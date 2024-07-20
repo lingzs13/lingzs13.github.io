2ae11b958213cb6585cfa782d1a50c486decce86
lingzs13 <112408695+lingzs13@users.noreply.github.com>
2024-07-20 13:27:58 +0000

    pwa实现 Updated By Github Actions

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/50857/","posts/14369/","posts/21118/","posts/40486/","posts/26940/","posts/174/","posts/49263/","posts/36171/","posts/13529/","posts/2607/","posts/5401/"];function toRandomPost(){
+var posts=["posts/26940/","posts/174/","posts/49263/","posts/36171/","posts/13529/","posts/2607/","posts/5401/","posts/40486/","posts/50857/","posts/14369/","posts/21118/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
