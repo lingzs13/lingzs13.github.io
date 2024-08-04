9d1cda947cb3af7946648acb832d3d41414abe4f
lingzs13 <112408695+lingzs13@users.noreply.github.com>
2024-08-04 05:30:49 +0000

    更新个人网站配置和文章，新增开源下载器aria2安装及配置指南
    本次提交主要更新了我的个人网站配置以及新增了一篇关于开源下载器aria2的安装及配置指南。具体包括以下几点：
    
    1. 更新了网站配置文件`_config.anzhiyu.yml`，新增了多吉云的CDN加速信息，同时移除了又拍云和Github托管的相关信息。
    2. 更新了`cdn.js`文件，将CDN缓存刷新的URL模式从单个页面扩展到整个站点的所有页面。
    3. 在`.obsidian/plugins/obsidian-image-auto-upload-plugin/data.json`文件中新增了远程服务器模式的设置，并上传了一些图片。
    4. 在`.obsidian/workspace.json`文件中更新了当前活跃文件和最后打开的文件列表。
    5. 新增了一篇关于开源下载器aria2的安装及配置指南，提供了详细的步骤和配置文件示例，以帮助用户在Windows环境下快速上手aria2。
    
    这次更新旨在优化网站的性能和用户体验，通过引入新的CDN服务和优化资源管理，提升了网站的加载速度和可用性。同时，通过新增关于aria2的教程，为用户提供了一个高效的下载工具的使用指南，丰富了网站的内容。 Updated By Github Actions

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/21118/","posts/40486/","posts/50857/","posts/14369/","posts/36171/","posts/13529/","posts/2607/","posts/5401/"];function toRandomPost(){
+var posts=["posts/21118/","posts/40486/","posts/50857/","posts/14369/","posts/35411/","posts/36171/","posts/13529/","posts/2607/","posts/5401/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
