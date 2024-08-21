ba70003c8ef2ac571007d2f33bb79168365b92d6
lingzs13 <112408695+lingzs13@users.noreply.github.com>
2024-08-21 16:41:53 +0000

    1 Updated By Github Actions

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,1 @@
-var posts=["posts/21118/","posts/40486/","posts/31322/","posts/40486/","posts/50857/","posts/14369/","posts/35411/","posts/36171/","posts/13529/","posts/2607/","posts/5401/"];function toRandomPost(){
-    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
-  };
+var posts=["posts/21118/","posts/40486/","posts/31322/","posts/40486/","posts/50857/","posts/14369/","posts/35411/","posts/36171/","posts/13529/","posts/2607/","posts/5401/"];function toRandomPost(){pjax.loadUrl("/"+posts[Math.floor(Math.random()*posts.length)])}
